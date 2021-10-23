9ab089f692d69fe90db4cfe06aed831fb4418e62
Yash-Singh1 <saiansh2525@gmail.com>
2021-10-23 13:18:01 -0700

    Improve not found page tests

--- src/components/NotFound.test.js
+++ src/components/NotFound.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ReactRouterDOM from 'react-router-dom';
 import NotFound from './NotFound';
+jest.mock('react-router-dom');
 
 describe('NotFound', () => {
   const notFound = shallow(<NotFound />);
 
   it('renders properly', () => {
-    expect(notFound).toMatchSnapshot();
+    expect(notFound.html()).toMatchSnapshot();
+  });
+
+  it('has a link', () => {
+    expect(ReactRouterDOM.Link.render).toHaveBeenCalled();
   });
 });
